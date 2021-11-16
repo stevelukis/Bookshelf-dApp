d8b62f3412cffa6094947589e7bcbb05b1815313
Steve Lukis <stevelukis88@gmail.com>
2021-11-16 15:51:48 +0800

    Add function to add book, get finished and unfinished book

---
+++ solidity/contracts/Bookshelf.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract Bookshelf is Ownable {
+
+    event AddBook(address recipient, uint bookId);
+
+    struct Book {
+        string name;
+        uint year;
+        string author;
+        bool finished;
+    }
+
+    Book[] private bookList;
+    mapping(uint256 => address) bookToOwner;
+
+    function addBook(string memory name, uint16 year, string memory author, bool finished) external {
+        bookList.push(Book(name, year, author, finished));
+        uint bookId = bookList.length - 1;
+        bookToOwner[bookId] = msg.sender;
+        emit AddBook(msg.sender, bookId);
+    }
+
+    function getUnfinishedBooks() external view returns (Book[] memory) {
+        Book[] memory temporary = new Book[](bookList.length);
+        uint counter = 0;
+        for (uint i = 0; i < bookList.length; i++) {
+            if (bookToOwner[i] == msg.sender) {
+                if (!bookList[i].finished) {
+                    temporary[counter] = bookList[i];
+                    counter++;
+                }
+            }
+        }
+
+        Book[] memory result = new Book[](counter);
+        for (uint i = 0; i < counter; i++) {
+            result[i] = temporary[i];
+        }
+        return result;
+    }
+
+    function getFinishedBooks() external view returns (Book[] memory) {
+        Book[] memory temporary = new Book[](bookList.length);
+        uint counter = 0;
+        for (uint i = 0; i < bookList.length; i++) {
+            if (bookToOwner[i] == msg.sender) {
+                if (bookList[i].finished) {
+                    temporary[counter] = bookList[i];
+                    counter++;
+                }
+            }
+        }
+
+        Book[] memory result = new Book[](counter);
+        for (uint i = 0; i < counter; i++) {
+            result[i] = temporary[i];
+        }
+        return result;
+    }
+
+}
--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,6 +1,5 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.0",
 };
